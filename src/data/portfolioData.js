9d8d36f0c0131abe4b2f3aa960e30af5f03272b1
Jean Christophe Gomis <devdujour@yahoo.com>
2021-08-19 10:18:59 +0200

    modification du fichier portfolio data

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -2,7 +2,7 @@ export const portfolioData = [
   {
     id: 1,
     name: 'JC-portfolio',
-    languages: ['javascript','html','css'],
+    languages: ['javascript', 'html', 'css'],
     languagesIcons: ['fab fa-js'],
     source: 'https://github.com/christophegomis89/html-css-js',
     info: 'Mes premiers pas dans le code avec la création de mon portfolio en Html&css , bootstrap .Auteur Jean Christophe Gomis',
@@ -11,7 +11,7 @@ export const portfolioData = [
   {
     id: 2,
     name: 'awara-boutique',
-    languages: ['javascript','php','symfony'],
+    languages: ['javascript', 'php', 'symfony'],
     languagesIcons: ['fab fa-php'],
     source: 'https://github.com/christophegomis89/awara-boutique',
     info: 'Projet php ,symfony Site de présentation des monument des grandes métropole à travaers le monde où vous pouvez vous connecter et partager vos éxpériences de voyages ',
@@ -20,7 +20,7 @@ export const portfolioData = [
   {
     id: 3,
     name: 'Projet-angular',
-    languages: ['javascript','angular'],
+    languages: ['javascript', 'angular'],
     languagesIcons: ['fab fa-angular'],
     source: 'https://github.com/christophegomis89/gsportnews_angular',
     info: 'Site d\'information sportif ,presentation de la 3e compétences de la formation DWWM-2020 à l\'afpa de Créteil  Auteur Jean Christophe Gomis.',
@@ -28,19 +28,19 @@ export const portfolioData = [
   },
   {
     id: 4,
-    name: 'gsportnews.fr',
-    languages: ['javascript','react'],
+    name: 'smblsemedo.com',
+    languages: ['javascript', 'react'],
     languagesIcons: ['fab fa-wordpress'],
-    source: 'http://www.gsportnews.fr',
+    source: 'http://www.smblsemedo.com',
     info: 'Site d\'iformation sportif :Presentation de la la 4e compétences de la formation DWWM -03 -2020 à l\'afpa de Créteil  : Auteur Jean Christophe Gomis',
     picture: './media/football.jpg'
   },
   {
     id: 5,
-    name: 'Projet -nodeJs',
-    languages: ['javascript','html','css'],
-    languagesIcons: ['fab fa-node-js'],
-    source: 'https://github.com/JC-20/JC-NODEJS'
+    name: 'Symfony',
+    languages: ['javascript', 'html', 'css'],
+    languagesIcons: ['fab fa-php'],
+    source: 'https://github.com/christophegomis89/projet-Symfony-BD'
     ,
     info: 'Projet backend Nodejs site de dessin animés :Auteur Jean Christophe Gomis ',
     picture: './media/pokemon.jpg'
@@ -48,8 +48,8 @@ export const portfolioData = [
   {
     id: 6,
     name: 'Projet react',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-react'],
+    languages: ['javascript', 'react'],
+    languagesIcons: ['fab fa-js', 'fab fa-react'],
     source: 'https://gsportnews.fr',
     info: 'Projet react représentation de mon cv  Auteur: Jean Christophe Gomis.',
     picture: './media/OM.JPG'
